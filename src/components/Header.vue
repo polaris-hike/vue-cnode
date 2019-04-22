7436648b8e843b6b0a621b56deee04549934d514
wuxuweilalala <46814017+wuxuweilalala@users.noreply.github.com>
2019-04-22 17:48:57 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
         <img src="../assets/cnodejs_light.svg" alt="">
       </router-link>
       <ul>
-        <li><a href="#">首页</a></li>
+        <li><a href="#">首页1</a></li>
         <li><a href="#">新手入门</a></li>
         <li><a href="#">API</a></li>
         <li><a href="#">关于</a></li>
